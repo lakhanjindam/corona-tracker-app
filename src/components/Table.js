c143ed9f0d57d38ebb0b29b7b8cf2ae411f973d6
lakhanjindam <lakhanj569@gmail.com>
2020-08-18 23:42:35 +0530

    Modified css and logic.

--- src/components/Table.js
+++ src/components/Table.js
@@ -20,7 +20,7 @@ const Table = ({ countries, types, darkMode }) => {
         : types === "recovered"
         ? countries.map(({ country, recovered }) => {
             return (
-              <tr>
+              <tr className={`${darkMode && "table__dark"}`}>
                 <td>{country}</td>
                 <td>
                   <strong>{numeral(recovered).format("0,0")}</strong>
@@ -30,7 +30,7 @@ const Table = ({ countries, types, darkMode }) => {
           })
         : countries.map(({ country, deaths }) => {
             return (
-              <tr>
+              <tr className={`${darkMode && "table__dark"}`}>
                 <td>{country}</td>
                 <td>
                   <strong>{numeral(deaths).format("0,0")}</strong>
